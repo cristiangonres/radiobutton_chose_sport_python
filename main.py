273f38a53a1023b1cfe3c617dfe4da5a0dd9b578
cristian <gonzalez.resina@gmail.com>
2022-12-06 20:58:52 +0100

    Correcciones

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from tkinter import *
 root = Tk()
 
+
+respuesta_label = Label(root, text='').grid(row=4) 
 pregunta = Label(root, text='¿Qué deporte te gusta más?').grid(row=0)
 respuesta = StringVar(value=" ")
 
 def reset_option():
     print('A tomar por culo')
     respuesta.set(None)
-
-
+    option_select('0')
+    
 def option_select(value):
-    value = 'Tu respuesta es ' + value
+    if value == '0':
+        value = 'No ha seleccionado'
+    else:
+        value = 'Tu respuesta es ' + value
     respuesta_label = Label(root, text=value).grid(row=4)
     
 opcion1 = Radiobutton(root, text='Muay Thai', value='Muay Thai', variable = respuesta, command=lambda : option_select(respuesta.get())).grid(row=1, sticky=W)
